26108965855f9c8cf151f909f0deccf1c9abcdb4
Hadi Santoso <hadisnt15@gmail.com>
2026-07-03 11:05:21 +0800

    update query vdate

--- view_dates.sql
+++ view_dates.sql
@@ -1,14 +1,16 @@
-CREATE VIEW vdates AS 
+CREATE OR REPLACE VIEW vdates AS
 
 WITH RECURSIVE dates AS (
-    SELECT DATE('2026-01-01') AS tanggal
+    SELECT DATE('2026-07-01') AS tanggal
+
     UNION ALL
+
     SELECT tanggal + INTERVAL 1 DAY
     FROM dates
-    WHERE tanggal < '2026-03-31'
+    WHERE tanggal < CURDATE()
 )
 
 SELECT
-tanggal,
-DAYNAME(tanggal) AS hari
+    tanggal,
+    DAYNAME(tanggal) AS hari
 FROM dates;
